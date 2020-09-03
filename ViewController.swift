2627635663ba249685125475ba1a2033a9342896
Jessica Mitchell <42589541+MITCHELLNJESS@users.noreply.github.com>
2020-09-03 18:14:45 -0400

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix App
+//
+//  Created by jessica mitchell on 9/3/20.
+//  Copyright © 2020 Jessica Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
