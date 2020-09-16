b7de28185361e30d5eeef1a2fffc81184bdd264a
Jessica Mitchell <42589541+MITCHELLNJESS@users.noreply.github.com>
2020-09-16 15:15:19 -0400

    Add files via upload

---
+++ MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix App
+//
+//  Created by jessica mitchell on 9/15/20.
+//  Copyright © 2020 Jessica Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
